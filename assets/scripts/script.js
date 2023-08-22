38dfa703f52d9d7f63ef567ba6434255d02aa71a
kepucino <okninskikarol2@gmail.com>
2023-08-22 11:42:13 +0200

    deleted:    assets/bootstrap-4.0.0-dist/css/bootstrap.min.css 	deleted:    assets/bootstrap-4.0.0-dist/css/bootstrap.min.css.map 	deleted:    assets/bootstrap-4.0.0-dist/js/bootstrap.min.js 	deleted:    assets/bootstrap-4.0.0-dist/js/bootstrap.min.js.map 	deleted:    assets/images/chakalaka.jpg 	deleted:    assets/images/dom0.jpg 	deleted:    assets/images/dom1.jpg 	deleted:    assets/images/dom2.jpg 	deleted:    assets/images/dom3.jpg 	deleted:    assets/images/dom4.png 	deleted:    assets/images/dom5.jpg 	deleted:    assets/images/dom6.jpg 	deleted:    assets/images/facebook.png 	deleted:    assets/images/instagram.png 	deleted:    assets/images/left-arrow.png 	deleted:    assets/images/logo.png 	deleted:    assets/images/magazyn0.jpg 	deleted:    assets/images/magazyn1.jpeg 	deleted:    assets/images/mail-icon.png 	deleted:    assets/images/oszczedzanie.jpeg 	deleted:    assets/images/panele0.png 	deleted:    assets/images/panele1.jpg 	deleted:    assets/images/pompa0.jpg 	deleted:    assets/images/pompa1.jpeg 	deleted:    assets/images/right-arrow.png 	deleted:    assets/images/telephone.png 	deleted:    assets/images/tlo2.png 	deleted:    assets/images/twitter.svg 	deleted:    assets/images/user.png 	deleted:    assets/jqueary/jquery-3.2.1.min.js 	modified:   assets/scripts/script.js 	modified:   assets/styles/style.css 	modified:   index.html 	deleted:    informacje.html 	deleted:    old/assets/images/1.jpg 	deleted:    old/assets/images/2.png 	deleted:    old/assets/styles/style.css 	deleted:    old/index.html 	deleted:    wycena.html

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,13 +1,56 @@
-document.querySelectorAll('a[href^="#skok"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+const xArray = ["Italy", "France", "Spain", "USA", "Argentina"];
+const yArray = [55, 49, 44, 24, 15];
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
+const data = [{
+  x:xArray,
+  y:yArray,
+  type:"bar",
+  orientation:"v",
+  marker: {color:"rgba(0,0,255,0.6)"}
+}];
+
+const layout = {title:"World Wide Wine Production"};
+
+Plotly.newPlot("myPlot", data, layout);
+
+
+// input fields
+
+const clearButton = document.querySelector('.trailing.clear');
+const form20 = document.querySelector("#form20");
+
+const showElement = (element) => {
+  if (element.classList.contains('d-none')) {
+    element.classList.remove('d-none');
+  }
+}
+
+const hideElement = (element) => {
+  if (!element.classList.contains('d-none')) {
+    element.classList.add('d-none');
+  }
+}
+
+const clearInput = (button) => {
+    const evt = document.createEvent("HTMLEvents");
+    evt.initEvent("blur", false, true);
+
+    const input = button.parentNode.querySelector(".form-icon-trailing");
+    input.value = null;
+    input.dispatchEvent(evt);
+    hideElement(button);
+}
+
+clearButton.addEventListener('click', () => clearInput(clearButton));
+clearButton.addEventListener('keydown', (event) => {
+  if (event.code === "Enter") {
+    event.preventDefault();
+    clearButton.click();
+  }
 });
 
-$('#slider-oferta').on('slide.bs.carousel', function () {
-    alert("siema")
+form20.addEventListener('input', () => {
+  if (form20.value !== null) {
+    showElement(clearButton);
+  }
 })
